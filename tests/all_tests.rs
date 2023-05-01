46d04a33de42acfd648141067ed6304c3883e86f
Nico Lehmann <nico.lehmannm@gmail.com>
2023-05-01 10:46:29 -0700

    Add even_odd and fact tests

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -3,42 +3,25 @@ mod infra;
 // Your tests go here!
 success_tests! {
     {
-        name: false_val,
-        file: "false_val.snek",
-        expected: "false",
-    },
-    {
-        name: input_compare_1,
-        file: "input_compare.snek",
-        input: "2",
-        expected: "false",
-    },
-    {
-        name: input_compare_2,
-        file: "input_compare.snek",
+        name: fact,
+        file: "fact.snek",
         input: "10",
-        expected: "true",
-    },
-}
-
-runtime_error_tests! {
-    {
-        name: invalid_argument,
-        file: "invalid_argument.snek",
-        expected: "invalid argument",
+        expected: "3628800",
     },
     {
-        name: input_compare_3,
-        file: "input_compare.snek",
-        input: "true",
-        expected: "invalid argument",
+        name: even_odd_1,
+        file: "even_odd.snek",
+        input: "10",
+        expected: "10\ntrue\ntrue",
     },
-}
-
-static_error_tests! {
     {
-        name: number_bounds_fail,
-        file: "number_bounds_fail.snek",
-        expected: "Invalid",
+        name: even_odd_2,
+        file: "even_odd.snek",
+        input: "9",
+        expected: "9\nfalse\nfalse",
     }
 }
+
+runtime_error_tests! {}
+
+static_error_tests! {}
